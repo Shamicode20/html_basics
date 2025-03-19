83f70347178128c3f2f11c0c1c473b912210d47d
Shamicode20 <seancatian@gmail.com>
2025-03-19 21:59:56 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$deal = 78;
+
+echo $deal;
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New World</title>
+</head>
+<body>
+    
+    
+    <p>Hello New Worluuueed</p>
+    <h1>HHAHAttHAHA??!!</h1>
+    <p>lolololo</p>
+    <h2>bagong laptop eyy</h2>
+</body>
+</html>
